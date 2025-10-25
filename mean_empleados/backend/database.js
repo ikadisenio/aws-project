b067b01203e048a37906bbc87816d778527136d2
Estudio Ika <ika.disenio@gmail.com>
2025-10-25 10:51:50 -0500

    update

--- mean_empleados/backend/database.js
+++ mean_empleados/backend/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const URI='mongodb://127.0.0.1/usuario_db';
+//const URI='mongodb://127.0.0.1/usuario_db';
+const URI='mongodb://atlas-sql-68e155dc13815444fb3864e3-gfizha.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin';
 mongoose.connect(URI)
 .then(db=> console.log('BD conectada'))
 .catch(err => console.error(err));
